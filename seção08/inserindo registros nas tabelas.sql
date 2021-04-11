b0a2139a39400bb2b1d220a84374a68b64b17de8
Wanderson Machado <wanmachadojr@gmail.com>
2021-04-11 20:22:07 -0300

    Modulo08

---
+++ seção08/inserindo registros nas tabelas.sql
@@ -0,0 +1,76 @@
+
+/* INSERINDO REGISTROS NA TABELA CLIENTE */
+
+INSERT INTO CLIENTE VALUES(NULL,'JOAO','M','JOAO@IG.COM','76567587887');
+INSERT INTO CLIENTE VALUES(NULL,'CARLOS','M','CARLOSA@IG.COM','5464553466');
+INSERT INTO CLIENTE VALUES(NULL,'ANA','F','ANA@IG.COM','456545678');
+INSERT INTO CLIENTE VALUES(NULL,'CLARA','F',NULL,'5687766856');
+INSERT INTO CLIENTE VALUES(NULL,'JORGE','M','JORGE@IG.COM','8756547688');
+INSERT INTO CLIENTE VALUES(NULL,'CELIA','M','JCELIA@IG.COM','5767876889');
+
++-----------+--------+------+----------------+-------------+
+| IDCLIENTE | NOME   | SEXO | EMAIL          | CPF         |
++-----------+--------+------+----------------+-------------+
+|         1 | JOAO   | M    | JOAO@IG.COM    | 76567587887 |
+|         2 | CARLOS | M    | CARLOSA@IG.COM | 5464553466  |
+|         3 | ANA    | F    | ANA@IG.COM     | 456545678   |
+|         4 | CLARA  | F    | NULL           | 5687766856  |
+|         5 | JORGE  | M    | JORGE@IG.COM   | 8756547688  |
+|         6 | CELIA  | M    | JCELIA@IG.COM  | 5767876889  |
++-----------+--------+------+----------------+-------------+
+6 rows in set (0.00 sec)
+
+
+
+/* INSERINDO REGISTROS NA TABELA ENDEREÇO */
+
+INSERT INTO ENDERECO VALUES(NULL,'RUA ANTONIO SA','CENTRO','B. HORIZONTE','MG',4);
+INSERT INTO ENDERECO VALUES(NULL,'RUA CAPITAO HERMES','CENTRO','RIO DE JANEIRO','RJ',1);
+INSERT INTO ENDERECO VALUES(NULL,'RUA PRES VARGAS','JARDINS','SAO PAULO','SP',3);
+INSERT INTO ENDERECO VALUES(NULL,'RUA ALFANDEGA','ESTACIO','RIO DE JANEIRO','RJ',2);
+INSERT INTO ENDERECO VALUES(NULL,'RUA DO OUVIDOR','FLAMENGO','RIO DE JANEIRO','RJ',6);
+INSERT INTO ENDERECO VALUES(NULL,'RUA URUGUAIANA','CENTRO','VITORIA','ES',5);
+
++------------+--------------------+----------+----------------+--------+------------+
+| IDENDERECO | RUA                | BAIRRO   | CIDADE         | ESTADO | ID_CLIENTE |
++------------+--------------------+----------+----------------+--------+------------+
+|          1 | RUA ANTONIO SA     | CENTRO   | B. HORIZONTE   | MG     |          4 |
+|          2 | RUA CAPITAO HERMES | CENTRO   | RIO DE JANEIRO | RJ     |          1 |
+|          3 | RUA PRES VARGAS    | JARDINS  | SAO PAULO      | SP     |          3 |
+|          4 | RUA ALFANDEGA      | ESTACIO  | RIO DE JANEIRO | RJ     |          2 |
+|          5 | RUA DO OUVIDOR     | FLAMENGO | RIO DE JANEIRO | RJ     |          6 |
+|          6 | RUA URUGUAIANA     | CENTRO   | VITORIA        | ES     |          5 |
++------------+--------------------+----------+----------------+--------+------------+
+6 rows in set (0.00 sec)
+
+
+
+/* INSERINDO REGISTROS NA TABELA TELEFONE */
+
+INSERT INTO TELEFONE VALUES(NULL,'CEL','78458743',5);
+INSERT INTO TELEFONE VALUES(NULL,'RES','56576876',5);
+INSERT INTO TELEFONE VALUES(NULL,'CEL','87866896',1);
+INSERT INTO TELEFONE VALUES(NULL,'COM','54768899',2);
+INSERT INTO TELEFONE VALUES(NULL,'RES','99667587',1);
+INSERT INTO TELEFONE VALUES(NULL,'CEL','78989765',3);
+INSERT INTO TELEFONE VALUES(NULL,'CEL','99766676',3);
+INSERT INTO TELEFONE VALUES(NULL,'COM','66687899',1);
+INSERT INTO TELEFONE VALUES(NULL,'RES','89986668',5);
+INSERT INTO TELEFONE VALUES(NULL,'CEL','88687909',2);
+
++------------+------+----------+------------+
+| IDTELEFONE | TIPO | NUMERO   | ID_CLIENTE |
++------------+------+----------+------------+
+|          1 | CEL  | 78458743 |          5 |
+|          2 | RES  | 56576876 |          5 |
+|          3 | CEL  | 87866896 |          1 |
+|          4 | COM  | 54768899 |          2 |
+|          5 | RES  | 99667587 |          1 |
+|          6 | CEL  | 78989765 |          3 |
+|          7 | CEL  | 99766676 |          3 |
+|          8 | COM  | 66687899 |          1 |
+|          9 | RES  | 89986668 |          5 |
+|         10 | CEL  | 88687909 |          2 |
++------------+------+----------+------------+
+10 rows in set (0.00 sec)
+
